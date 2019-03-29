4dfaa6b41e9055beabc0b6344eeb3b606e92f8d1
Fábián Tamás László <giganetom@gmail.com>
2019-03-29 16:22:54 +0100

    Trying to satisfy Travis...

--- .travis/install.sh
+++ .travis/install.sh
@@ -6,4 +6,14 @@ yes "" | pecl install mongodb
 echo 'extension = apcu.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 echo 'apc.enabled = 1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
 echo 'apc.enable_cli = 1' >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+
+if php -r 'exit(extension_loaded("mongodb")?0:1);'
+then
+    echo "The 'mongodb' extension is already loaded into PHP, skipping installation."
+else
+    echo "Installing mongodb extension into php..."
+    echo 'extension = mongodb.so'  >> ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/travis.ini
+    echo "...done."
+fi
+
 ./composer.phar install
